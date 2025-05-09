8a815c1d3388b8a5b67c9be0617d077354728cd0
suriya0015 <aizensasuke0097@gmail.com>
2025-05-09 17:40:24 +0530

    'us'

--- Mainapp/urls.py
+++ Mainapp/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/contact', views.contact, name='contact'),
-    path('/register', views.register, name='register'),
-    path('/login', views.login, name='login'),
-    path('/todolist', views.todolist, name='todolist'),
+    path('contact/', views.contact, name='contact'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('todolist/', views.todolist, name='todolist'),
 
 ]
