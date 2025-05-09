c646417307a63f507e03bd3a41a25d5c7417d0a0
suriya0015 <aizensasuke0097@gmail.com>
2025-05-09 17:16:12 +0530

    'vercel'

--- taskmanagement/settings.py
+++ taskmanagement/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-agaf%a^+r!)%3)p5eznzzoz3$k17z=i-oiw$9i7_)!*o1l6r$p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'taskmanagement.wsgi.application'
+WSGI_APPLICATION = 'taskmanagement.wsgi.app'
 
 
 # Database
