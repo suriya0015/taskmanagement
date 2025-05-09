996b5e4605b7f2e8572f71d9381246811253d84f
suriya0015 <aizensasuke0097@gmail.com>
2025-05-09 17:45:22 +0530

    'us1'

--- taskmanagement/settings.py
+++ taskmanagement/settings.py
@@ -25,9 +25,14 @@ SECRET_KEY = 'django-insecure-agaf%a^+r!)%3)p5eznzzoz3$k17z=i-oiw$9i7_)!*o1l6r$p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# Vercel app wor
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Use Path for STATIC_ROOT
 
 
 # Application definition
