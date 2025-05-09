457186f228376faa94973612de093b33725a642e
suriya0015 <aizensasuke0097@gmail.com>
2025-05-09 16:47:48 +0530

    'push'

--- Mainapp/urls.py
+++ Mainapp/urls.py
@@ -4,8 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('/contact', views.contact, name='contact'),
+    
     path('/register', views.register, name='register'),
     path('/login', views.login, name='login'),
     path('/todolist', views.todolist, name='todolist'),
-    
+
 ]
