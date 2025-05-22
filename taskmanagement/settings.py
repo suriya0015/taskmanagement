d8e027aa7501fc8b2fede6b092a76d8e09964b93
suriya0015 <aizensasuke0097@gmail.com>
2025-05-22 19:05:35 +0530

    ers

--- taskmanagement/settings.py
+++ taskmanagement/settings.py
@@ -122,9 +122,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # or BASE_DIR / 'staticfiles' if using Path
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
