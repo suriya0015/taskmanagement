9cb5aa10d7dee3d61d3a78a91e4d55e501bdf117
suriya0015 <aizensasuke0097@gmail.com>
2025-05-22 18:54:53 +0530

    staticfiles works but changing it to static

--- taskmanagement/settings.py
+++ taskmanagement/settings.py
@@ -123,7 +123,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
