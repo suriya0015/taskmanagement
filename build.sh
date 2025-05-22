533625c5c48602c773b69cd7ffb70d48cc8d4a05
suriya0015 <aizensasuke0097@gmail.com>
2025-05-22 17:52:02 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -19,4 +19,7 @@ python3.9 -m pip install -r requirements.txt
 # collect static files into the staticfiles directory
 python3 manage.py collectstatic --noinput
 
+mkdir -p static
+cp -r staticfiles/* static/
+
 echo "BUILD END"
